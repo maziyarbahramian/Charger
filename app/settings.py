21c12b71db71e1f7e071a6c382b75dee08ccd648
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2024-02-03 09:16:39 +0330

    add mysql config

--- app/settings.py
+++ app/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tabdeal',
+        'USER': 'root',
+        'HOST': '127.17.0.2',
+        'PASSWORD': 'mysql',
+        'PORT': '3306',
     }
 }
 
